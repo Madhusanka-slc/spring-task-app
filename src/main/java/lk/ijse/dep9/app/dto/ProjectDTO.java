568ed3e0f05cbf35a2716a93b30ca58c0f6df39d
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-12-22 14:12:29 +0530

    Implement Project Controller

--- src/main/java/lk/ijse/dep9/app/dto/ProjectDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/ProjectDTO.java
@@ -20,4 +20,6 @@ public class ProjectDTO implements Serializable {
     @NotBlank(message = "Project name can't be null or empty")
     @Length(min = 3,message = "Project name should be at least 3 characters long")
     private String name;
+
+    private String username;
 }
--- src/main/java/lk/ijse/dep9/app/service/custom/ProjectTaskService.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/ProjectTaskService.java
@@ -1,4 +1,17 @@
 package lk.ijse.dep9.app.service.custom;
 
+import lk.ijse.dep9.app.dto.ProjectDTO;
+
+import java.util.List;
+
 public interface ProjectTaskService {
+    ProjectDTO createNewProject(ProjectDTO projectDTO);
+
+    List<ProjectDTO> getAllProjects(String username);
+
+    ProjectDTO getProjectDetails(String username, Integer projectId);
+
+    void renameProject(ProjectDTO project);
+
+    void deleteProject(String username, int projectId);
 }
